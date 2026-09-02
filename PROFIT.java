f7880bec1126491790aeac1b6d24f1cba95273eb
Pearl Charish Lumacad Helia <pearlyhelia@gmail.com>
2026-09-02 22:58:48 +0800

    Toms

--- PROFIT.java
+++ PROFIT.java
@@ -3,6 +3,7 @@ package SARISARISTORE;
 public class PROFIT {
 public static void main(String[] args) {
         System.out.println("Repository");
+        System.out.println("Will code tommorrow, today is the best time to work on this");
 }
     
 }
