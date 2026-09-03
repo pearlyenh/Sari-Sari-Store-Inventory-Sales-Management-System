17e96f4fe93f15c04ec5f2cd69ca4734816a5e85
Pearl Charish Lumacad Helia <pearlyhelia@gmail.com>
2026-09-03 21:52:03 +0800

    D

--- PROFIT.java
+++ PROFIT.java
@@ -4,6 +4,7 @@ public class PROFIT {
 public static void main(String[] args) {
         System.out.println("Repository");
         System.out.println("Will code tommorrow, today is the best time to work on this");
+        System.out.println("Tommorrow I promise");
 }
     
 }
