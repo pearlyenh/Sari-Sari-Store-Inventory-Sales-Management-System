0eb87be35d73120a3c735d96a05a18c45e54391d
Pearl Charish Lumacad Helia <pearlyhelia@gmail.com>
2026-09-07 23:19:30 +0800

    ok

--- PROFIT.java
+++ PROFIT.java
@@ -6,11 +6,14 @@ public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
 
-        System.out.println("Repository");
-        String repost = scanner.next();
-        System.out.println("Hello world");
+        System.out.println("Enter text: ");
+        String repost = scanner.nextLine();
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+
 
         System.out.println(repost);
+        System.out.println(number);
 
         scanner.close();
 }
