cbcddaba98c06128208d2da5ca65b2bc1692eab3
Pearl Charish Lumacad Helia <pearlyhelia@gmail.com>
2026-09-08 11:35:54 +0800

    Repository to do list

--- MAIN.java
+++ MAIN.java
@@ -3,5 +3,12 @@ package SARISARISTORE;
 public class MAIN{
     public static void main(String [] args){
         System.out.println("Quick reposity purposes");
+        System.out.println("TO DO LIST");
+        System.out.println("[1] Finish GCR");
+        System.out.println("[2] Quick repository");
+        System.out.println("[3] Study for STS");
+        System.out.println("[4] Study for DSA");
+        System.out.println("{5] Prepare for things tomorrow");
+        System.out.println("[6] Rizal Quick Notes for active oral recitation");
     }
 }
