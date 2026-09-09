cdb6925b8355df25e4a8331722c41a927a1b0e09
Pearl Charish Lumacad Helia <pearlyhelia@gmail.com>
2026-09-09 23:00:50 +0800

    Add

--- MAIN.java
+++ MAIN.java
@@ -1,7 +1,9 @@
 package SARISARISTORE;
+import java.util.Scanner;
 // product first 
 public class MAIN{
     public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Quick reposity purposes");
         System.out.println("TO DO LIST");
         System.out.println("[1] Finish GCR");
@@ -10,5 +12,16 @@ public class MAIN{
         System.out.println("[4] Study for DSA");
         System.out.println("{5] Prepare for things tomorrow");
         System.out.println("[6] Rizal Quick Notes for active oral recitation");
+        System.out.println("Enter an option: ");
+        int option = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                System.out.println("You have done finish the GCR.");               
+                break;
+        
+            default:
+                break;
+        }
     }
 }
