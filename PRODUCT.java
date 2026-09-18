edfa4fc44877ba46f2823aa4959c9621a643e8ac
Pearl Charish Lumacad Helia <pearlyhelia@gmail.com>
2026-09-18 19:02:14 +0800

    D

--- PRODUCT.java
+++ PRODUCT.java
@@ -467,6 +467,7 @@ public class PRODUCT {
 
         System.out.println("\n===== RESTOCK SUMMARY =====");
         System.out.println("Previous Stock: " + foundProduct.getQuantity());
+        System.out.println("Bisaya version");
         System.out.println("New Stock Added: " + newQuantity);
         System.out.println("Updated Stock: " + combinedQuantity);
 
